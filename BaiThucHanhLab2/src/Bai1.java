556ac285c2cc0e4c14b04dbafc4363c9bdc7045f
Datnguyen1712 <124805203+Datnguyen1712@users.noreply.github.com>
2023-03-15 09:36:45 +0700

    conmmit note

---
+++ BaiThucHanhLab2/src/Bai1.java
@@ -0,0 +1,18 @@
+import java.util.scanner;
+public class Bai1{
+    public static void main (string[] args){
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap mot so bat ky n: ");
+        n = scanner.nextInt();
+        if (n % 2 == 0){
+            system.out.println(n + "La so chan");
+        }else {
+            system.out.println(n + "La so le");
+        }
+
+
+    }
+
+}
+
